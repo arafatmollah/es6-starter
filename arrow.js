8e3cbe421edc8d9cf5d07285e4db0fbf6d5d2716
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-10 11:45:31 +0600

    big arrow

--- arrow.js
+++ arrow.js
@@ -14,7 +14,23 @@ const add3 = function add3(num1, num2) {
 }
 const sum3 = add3(12, 34);
 console.log(sum, sum2, sum3);
+
+
 //arrow function
 const sumofnumber = (n1, n2) => n1 + n2;
 const sum4 = sumofnumber(12, 34);
-    console.log(sum4)
+console.log(sum4);
+
+const multiply = (num1, num2) => num1 * num2;
+const multiple = multiply(45, 3);
+console.log(multiple);
+
+const bigArrow = (x, y) => {
+    const added = x + y;
+    const differ = x - y;
+    const multi = added * differ;
+    const all = added + differ + multi;
+    return all;
+}
+const allFunc = bigArrow(23, 10);
+console.log(allFunc)
