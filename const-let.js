6ce5da221d141f278143efb82dec3943a1aeb35f
Arafat MShuvo <arafatshuvo18@gmail.com>
2022-04-10 11:25:35 +0600

    es6 started

---
+++ const-let.js
@@ -0,0 +1,8 @@
+const myName = 'arafat shuvo';
+console.log(myName)
+//never resign any value of const
+
+let fullname = 'arafat shuvo' + 13;
+fullname = 'arafat shuvo thats it'
+console.log(fullname)
+//you can reassign of value of let
